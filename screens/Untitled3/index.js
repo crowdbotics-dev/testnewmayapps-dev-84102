9fc8fff1b85ec77a22359bb9c9564f050b20bfa4
Ravinder sales <ravinder.y@crowdbotics.com>
2023-05-18 12:11:16 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled3/index.js
+++ screens/Untitled3/index.js
@@ -1,5 +1,6 @@
+import { Text } from "react-native";
 import React from "react";
-import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled3 = () => {
   return <SafeAreaView style={styles.safeArea}>
@@ -8,13 +9,27 @@ const Untitled3 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><View style={styles.TATtiMGv}></View><Text style={styles.lMLyxcup}>Lorem ipsum…</Text></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  TATtiMGv: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  lMLyxcup: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
   }
 });
 export default Untitled3;
